0f3dcfa89fe36f0a86693dc73009500f1e52ccb5
Jsusvalle <jesusvalle2001@hotmail.com>
2023-01-19 12:36:58 -0500

    fix: build images in public

--- src/helpers/icons.ts
+++ src/helpers/icons.ts
@@ -1,14 +1,14 @@
 export const Icons = [
   {
     name: 'Countries',
-    path: '/src/assets/images/map.png',
+    path: '/assets/images/map.png',
   },
   {
     name: 'Spotify',
-    path: '/src/assets/images/spotify.png',
+    path: '/assets/images/spotify.png',
   },
   {
     name: 'Prices',
-    path: '/src/assets/images/coin.png',
+    path: '/assets/images/coin.png',
   },
 ];
